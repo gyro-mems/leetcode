d73603a9653c3a10add8a7b337ed0ef434f846dd
gyro-mems <62749989+gyro-mems@users.noreply.github.com>
2020-03-27 07:45:04 -0500

    Update myatoi.cpp

--- myatoi.cpp
+++ myatoi.cpp
@@ -9,12 +9,17 @@ public:
 		int len = str.length();
 		int sign = 1;  // set to negative 1 if the value is negative
 		int total = 0;  // keeps track of what to output as the integer
-        
+		
+		// remove blank spaces
 		int i = 0;
 		while(i < len && str[i] == ' '){
 			i++;
 		}
+		if(i >= len) {  // this means that everything in the input string was blank
+			return total;
+		}
 		
+		// check the sign of the number
 		if(str[i] == '-'){
 			sign = -1;
 			i++;
@@ -23,9 +28,12 @@ public:
 			i++;
 		}
         
+		// go through the rest of the remaining string to build up the output number
 		for(i; i < len; i++){
-			if(48 <= (int)str[i] && (int)str[i] <= 57){  // does not include '0'
-				int next_digit = (int)str[i] - 48; 
+			// in ascii 48 - 57 are the values for numbers 0 - 9
+			if(48 <= (int)str[i] && (int)str[i] <= 57){  // the character is a number (0-9)
+				int next_digit = (int)str[i] - 48;  // subtracting by 48 puts the values of the ascii characters back into the range of 0 - 9  
+				
 				// mult section
 				if(sign == 1 && total != 0){
 					int max_multiplications = MAX/total;
@@ -36,7 +44,7 @@ public:
 					}
 				} else if(sign == -1 && total != 0){
 					int max_multiplications = MIN/total;
-					if(max_multiplications >= 10){
+					if(max_multiplications >= 10 || total == -1){
 						total *= 10;
 					} else {
 						return MIN;
